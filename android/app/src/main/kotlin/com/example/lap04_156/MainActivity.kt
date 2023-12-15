ce7a1911b451a1a81016ce6f212fff70f363d123
HarUIzukI <krotcast099@gmail.com>
2023-12-15 15:23:02 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/lap04_156/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lap04_156
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/inputpage.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String foodradio = "";
+  bool staregg = false;
+  bool boilegg = false;
+  bool pork = false;
+  bool chilly = false;
+  bool chick = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Input Widget"),
+        backgroundColor: Color.fromARGB(255, 191, 235, 252),
+      ),
+      body: Column(
+        children: [
+          radioMenulist("ก๋วยเตี๋ยว", "60 บาท"),
+          radioMenulist("คะน้าหมูกรอบ", "55 บาท"),
+          radioMenulist("ผัดพริกตับ", "65 บาท"),
+          radioMenulist("ข้าวผัด", "45 บาท"),
+          radioMenulist("ผัดผัก", "50 บาท"),
+          Divider(),
+          CheckboxListTile(
+            title: Text("ไข่ดาว"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: staregg,
+            onChanged: (value) {
+              setState(() {
+                staregg = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("ไข่ต้ม"),
+            secondary: Text("5 บาท"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: boilegg,
+            onChanged: (value) {
+              setState(() {
+                boilegg = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("หมูทอด"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: pork,
+            onChanged: (value) {
+              setState(() {
+                pork = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("ไก่ทอด"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: chick,
+            onChanged: (value) {
+              setState(() {
+                chick = value!;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text("พริก"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: chilly,
+            onChanged: (value) {
+              setState(() {
+                chilly = value!;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  RadioListTile<String> radioMenulist(String title, String subtitle) {
+    return RadioListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      activeColor: Colors.green,
+      value: title,
+      groupValue: foodradio,
+      onChanged: (value) {
+        setState(() {
+          foodradio = value!;
+        });
+        // print(title);
+      },
+    );
+  }
+}
